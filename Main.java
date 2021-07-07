f11977ba088424d1a9bd501b47c64a04808d3395
alokdubey01 <72490595+alokdubey01@users.noreply.github.com>
2021-07-07 10:49:55 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+ protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome_page);
